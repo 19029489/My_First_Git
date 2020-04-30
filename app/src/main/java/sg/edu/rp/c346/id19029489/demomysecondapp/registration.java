b055bcd9e99f6528909575ce261656e1245cb9e1
19029489 <19029489@rp.edu.sg>
2020-04-30 21:23:05 +0800

    Additional details

---
+++ app/src/main/java/sg/edu/rp/c346/id19029489/demomysecondapp/registration.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19029489.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
