a6d8cfd130de98df308f945cf5f85c5d9398bd66
NG HUAI YU <19029489@myrp.edu.sg>
2020-04-30 13:48:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19029489/demomysecondapp/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c346.id19029489.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+s
